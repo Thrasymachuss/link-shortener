38e26e765d572e72772030e6f153e6d1035ed853
Thrasymachuss <ryan.w.groch@gmail.com>
2022-11-27 17:29:00 -0600

    Switched to planetscale db

--- src/components/Dashboard/useDashboard.ts
+++ src/components/Dashboard/useDashboard.ts
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { trpc } from "../../utils/trpc";
 
 const useDashboard = () => {
-  // const searchState = useState("");
   useSession();
 
   const urlsQuery = trpc.shortUrl.getUrls.useQuery();
